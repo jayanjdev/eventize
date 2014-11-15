d8a083a466693b12e9d00ee777d80208912578cf
Jayan Jacob <jayandevjacob@gmail.com>
2014-11-15 13:28:39 -0500

    udpates configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   get 'anglr_app/index'
 
+  get 'graphs/events'
+
+  get '/events_graph', to: 'events#search'
+  get '/top_events', to: 'events#top_events'
+
   resources :events do 
     resources :tags 
   end
 
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  resources :users do
+  end
   
 end
